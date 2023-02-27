3ee09512e38f6a9d24694a2f82e616d36a230206
Polylyly <polybius404@gmail.com>
2023-02-27 21:39:44 +0200

    Build

---
+++ GOA Game Jam #1/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (SceneManager.GetActiveScene().name == "PastWorld")
+                SceneManager.LoadScene("FutureWorld");
+            if (SceneManager.GetActiveScene().name == "FutureWorld")
+                SceneManager.LoadScene("PastWorld");
+        }
+    }
+}
