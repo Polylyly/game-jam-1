2239220dc9880aebc9f10f0f5741e8657c3761a5
Isabel <dojofxseito@gmail.com>
2023-02-27 21:08:09 -0800

    Revert "Revert "Build""
    This reverts commit 12483e9e9cb7a01efd5ba9ddefa0ac9a5946d54a.

--- GOA Game Jam #1/Assets/Scripts/EggItem.cs
+++ GOA Game Jam #1/Assets/Scripts/EggItem.cs
@@ -7,16 +7,9 @@ public class EggItem : MonoBehaviour
 
     public static EggItem instance;
 
-    public Transform LeftHandEmpty;
-    public Transform RightHandEmpty;
-
-    public GameObject Egg;
-    private Transform spawnPoint;
-
-    private bool InLeftHand;
-    private bool InRightHand;
-
-    //private GameObject cloneEgg;
+    public GameObject LeftHandEmpty;
+    //Vector3 LeftHandPosition = LeftHandEmpty.transform.position;
+    public GameObject RightHandEmpty;
 
     private void Awake()
     {
@@ -32,36 +25,15 @@ public class EggItem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //cloneEgg = GameObject.FindGameObjectWithTag("CloneofEgg");
         
-        while (InLeftHand == true)
-        {
-            
-            //cloneEgg.transform.position = spawnPoint;
-        }
-        while (InRightHand == true)
-        {
-            
-            //cloneEgg.transform.position = spawnPoint;
-        }
     }
 
     public void GrabEggLeft()
     {
-        Debug.Log("test");
-        Instantiate(Egg, LeftHandEmpty);
-        InLeftHand = true;
 
     }
     public void GrabEggRight()
     {
-        Debug.Log("test");
-        Instantiate(Egg, RightHandEmpty);
-        InRightHand = true;
 
-    }
-    void cloneEgg()
-    {
-        Debug.Log("test");
     }
 }
